2a8b0ceeeaa2c1ce9c6bc6d2f227df2639c7f143
yamasy1549 <sanae@yamasy.info>
2016-06-20 17:43:12 +0900

    Add a twitter share button

--- src/scripts/all.js
+++ src/scripts/all.js
@@ -1,1 +1,7 @@
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-79565060-1', 'auto');ga('send', 'pageview');
+
+var url = encodeURIComponent(document.URL);
+var title = encodeURIComponent(document.title + " #kosen14s");
+var shareLink = "http://twitter.com/share?url=" + url + "&text=" + title;
+document.getElementById('twitter').href = shareLink;
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
